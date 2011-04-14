8195117c8f3fb4198bdf1dbe5974e78c5bc53e6e
jeff <leterip@gmail.com>
2011-04-14 10:23:53 -0400

    refactoring to make more sense

--- xbrltpl.py
+++ xbrltpl.py
@@ -82,26 +82,14 @@ class Template(object):
 			import pickle
 			new_template = pickle.loads(data)
 			self.__dict__.update(new_template.__dict__)
-			self._data = Matrix()
 			return
 		
 		self._contexts = []
 		self._facts = []
-		self._data = Matrix()
 	
 	def pickle(self):
 		import pickle
-		from contextlib import contextmanager
-
-		@contextmanager
-		def no_data_context():
-			data_backup = self._data
-			del self._data
-			yield
-			self._data = data_backup
-		
-		with no_data_context():
-			return pickle.dumps(self)
+		return pickle.dumps(self)
 	
 	@property
 	def contexts(self):
@@ -115,56 +103,8 @@ class Template(object):
 	def units(self):
 		return list(set(unit for fact, unit in self._facts))
 	
-	@property
-	def data(self):
-		temp_data = []
-		for row_idx, (fact, unit) in enumerate(self._facts):
-			temp_row = []
-			for col_idx, context in enumerate(self._contexts):
-				temp_row.append(self._data[row_idx, col_idx])
-			temp_data.append(temp_row)
-		return temp_data
-	
-	def pretty_data(self):
-		return '\n'.join(
-			'\t'.join(
-				item is None and '-' or item.__repr__() for item in row
-			)
-			for row in self.data
-		)
-
-	def get_row_col(self, fact, unit, context):
-		row = self._facts.index((fact, unit))
-		col = self._contexts.index(context)
-		return (row, col)
-	
 	def get_cell_info(self, row, col):
 		return self.facts[row], self.facts[col]
-	
-	def _transform_index(self, index):
-		if not isinstance(index, tuple):
-			raise KeyError('Invalid key')
-		row, col = index
-		if isinstance(row, tuple):
-			#We have a get based on things
-			row, col = self.get_row_col(row[0], row[1], col)
-		return row, col
-	
-	def __getitem__(self, index):
-		row, col = self._transform_index(index)
-		return self._data[row, col]
-	
-	def __setitem__(self, index, data):
-		row, col = self._transform_index(index)
-		#TODO: validation
-		#(fact, unit), context = self.get_cell_info(row, col)
-		#fact.validate(data), unit.validate(data), context.validate(data)
-
-		self._data[row, col] = data
-	
-	def __delitem__(self, index):
-		row, col = self._transform_index(index)
-		self._data[row, col] = self._data.default
 	
 	def add_fact(self, fact, unit):
 		if isinstance(fact, Fact) and isinstance(unit, Unit):
@@ -198,7 +138,6 @@ class Template(object):
 			pass
 		
 		del self._facts[row]
-		del self._data[row, None]
 	
 	def del_context(self, index):
 		col = index
@@ -208,5 +147,101 @@ class Template(object):
 			pass
 		
 		del self._contexts[col]
-		del self._data[None, col]
+
+class Filing(object):
+	"""Filing class holds a template and data to go in it"""
+
+	def __init__(self, with_template=None, with_data=None):
+		if with_template is not None:
+			self._template = with_template
+		else:
+			self._template = Template()
+		
+		if with_data is not None:
+			self._data = with_data
+		else:
+			self._data = Matrix()
+	
+	def pickle(self):
+		import pickle
+		return pickle.dumps(self)
+	
+	@property
+	def data(self):
+		temp_data = []
+		for row_idx, (fact, unit) in enumerate(self._facts):
+			temp_row = []
+			for col_idx, context in enumerate(self._contexts):
+				temp_row.append(self._data[row_idx, col_idx])
+			temp_data.append(temp_row)
+		return temp_data
+	
+	@property
+	def contexts(self):
+		return self._template.contexts
+	
+	@property
+	def facts(self):
+		return self._template.facts
+	
+	@property
+	def units(self):
+		return list(set(unit for fact, unit in self._template.facts))
+	
+	def pretty_data(self):
+		return '\n'.join(
+			'\t'.join(
+				item is None and '-' or item.__repr__() for item in row
+			)
+			for row in self.data
+		)
+	
+	def get_row_col(self, fact, unit, context):
+		row = self._facts.index((fact, unit))
+		col = self._contexts.index(context)
+		return (row, col)
+	
+	def _transform_index(self, index):
+		if not isinstance(index, tuple):
+			raise KeyError('Invalid key')
+		row, col = index
+		if isinstance(row, tuple):
+			#We have a get based on things
+			row, col = self.get_row_col(row[0], row[1], col)
+		return row, col
+	
+	def __getitem__(self, index):
+		row, col = self._transform_index(index)
+		return self._data[row, col]
+	
+	def __setitem__(self, index, data):
+		row, col = self._transform_index(index)
+		#TODO: validation
+		#(fact, unit), context = self.get_cell_info(row, col)
+		#fact.validate(data), unit.validate(data), context.validate(data)
+		
+		self._data[row, col] = data
+	
+	def __delitem__(self, index):
+		row, col = self._transform_index(index)
+		self._data[row, col] = self._data.default
+	
+	def del_fact(self, index):
+		row = index
+		try:
+			row = self._template._facts.index(index)
+		except ValueError:
+			pass
+		self._template.del_fact(row)
+		del self._data[row, None]
+
+	def del_context(self, index):
+		col = index
+		try:
+			col = self._template._contexts.index(index)
+		except ValueError:
+			pass
+		self._template.del_context(col)
+		del self._data[None, row]
+
 
